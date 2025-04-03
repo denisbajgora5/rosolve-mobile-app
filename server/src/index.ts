5b6e47c8afccb5361589c02178ad222eb5b30037
denis <altstarv@gmail.com>
2025-04-03 17:55:40 +0100

    Express server is added and working!

---
+++ server/src/index.ts
@@ -0,0 +1,15 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const PORT = 3002
+
+app.use(cors({ origin: 'http://localhost:19006' })) // only allow Expo Web
+
+app.get('/api/value', (req, res) => {
+  res.json({ value: 'Express Server Status: WORKING!' })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+})
