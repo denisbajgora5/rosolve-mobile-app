cf2fbed21cd1ba14383987af6d75295c80dbd3ca
denis <altstarv@gmail.com>
2025-04-10 14:35:56 +0100

    Fixed backend connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -25,5 +25,5 @@ app.get('/api/value', (req, res) => {
 app.use('/api/passenger', passengerRoutes)
 
 app.listen(PORT, () => {
-  console.log(`✅ Server running at http://10.156.26.90:${PORT}`)
+  console.log(`✅ Server running at ${process.env.EXPRESS_SERVER_IP}:${PORT}`)
 })
